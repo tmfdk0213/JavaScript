04599c69cf45302cd5e1a3fc48d03d475adecc63
seula <tmfdk0213@gmail.com>
2023-05-02 22:41:46 +0900

    '리액트''

--- JavaScript/20230427/vending/js/classes/colaGenerator.js
+++ JavaScript/20230427/vending/js/classes/colaGenerator.js
@@ -28,12 +28,12 @@ class ColaGenerator {
     data.forEach((el) => {
       const item = document.createElement("li");
       const itemTemplate = `
-            <button class="btn-cola" type="button" data-item="${el.name}" data-count="${el.count}" data-price="${el.cost}" data-img="${el.img}">
-                <img class="cola-img" src="./img/${el.img}" alt="">
-                <span class="cola-name">${el.name}</span>
-                <strong class="cola-price">${el.cost}원</strong>
-            </button>
-            `;
+          <button class="btn-cola" type="button" data-item="${el.name}" data-count="${el.count}" data-price="${el.cost}" data-img="${el.img}">
+              <img class="cola-img" src="./img/${el.img}" alt="">
+              <span class="cola-name">${el.name}</span>
+              <strong class="cola-price">${el.cost}원</strong>
+          </button>
+          `;
 
       item.innerHTML = itemTemplate;
       docFrag.append(item);
