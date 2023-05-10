954572ebd8f3bc1a1772fae761816ae4e62384c7
seula <tmfdk0213@gmail.com>
2023-05-10 16:44:59 +0900

    '리액트'

---
+++ React/20230509/myproject/src/Modal.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Modal.css";
+
+export default function Modal({ children }) {
+  return (
+    <div className="modal-backdrop">
+      <article className="modal">
+        {children}
+        {/* <h2>홈페이지에 오신것을 환영합니다!</h2>
+                <p>좋은하루 되세요!</p> */}
+        <button>닫기</button>
+      </article>
+    </div>
+  );
+}
